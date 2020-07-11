b65fd6b51db047f0a40a0fd88a2e4e68369571c8
Mona Lisa <mario@Marios-MacBook-Air.local>
2020-07-11 17:13:22 -0500

    ahi va

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,55 @@
+#import modules
+import csv
+import os
 
+# Specify the file to write to
+budgetpath = os.path.join("Resources", "budget_data.csv")
+
+# Open the file. Specify the variable to hold the contents
+with open(budgetpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvfile)
+
+    ProfitLoss = []
+    Periods = []
+    Variation = []
+    
+#read each row of file
+    for rows in csvreader:
+        ProfitLoss.append(int(rows[1]))
+        Periods.append(rows[0])
+
+    for x in range(1, len(ProfitLoss)):
+        Variation.append((int(ProfitLoss[x]) - int(ProfitLoss[x-1])))
+    
+#calculate average revenue
+    AveRevenue = sum(Variation) / len(Variation)
+    
+#calculate number of months in data
+    Months = len(Periods)
+
+#find greatest increase in profits
+    GrtIncrease = max(Variation)
+
+#find greatest decrease in profits
+    GrtDecrease = min(Variation)
+
+#print stats
+    print("Financial Analysis")
+    print("----------------------------")
+    print("Total Months: " + str(Months))
+    print("Total: $" + str(sum(ProfitLoss)))
+    print("Average  Change: $" + str("{:.2f}".format(AveRevenue)))
+    print("Greatest Increase in Profits: " + str(Periods[Variation.index(max(Variation))+1]) + " " + '$(' + str(GrtIncrease) + ')')
+    print("Greatest Decrease in Profits: " + str(Periods[Variation.index(min(Variation))+1]) + " " + '$(' + str(GrtDecrease) + ')')
+
+#write to file
+    output = open("Analysis/ProfitLossResults.txt", "w")
+    output.write("Financial Analysis"+"\n")
+    output.write("----------------------------"+"\n")
+    output.write("Total Months: " + str(Months)+"\n")
+    output.write("Total: $" + str(sum(ProfitLoss))+"\n")
+    output.write("Average  Change: $" + str("{:.2f}".format(AveRevenue))+"\n")
+    output.write("Greatest Increase in Profits: " + str(Periods[Variation.index(max(Variation))+1]) + " " + '$(' + str(GrtIncrease) + ')'+"\n")
+    output.write("Greatest Decrease in Profits: " + str(Periods[Variation.index(min(Variation))+1]) + " " + '$(' + str(GrtDecrease) + ')'+"\n")
+    output.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,52 @@
+#import modules
+import csv
+import os
+# Specify the file to write to
+pollpath = os.path.join("Resources", "election_data.csv")
+#Create variable to count number of votes
+#Create lists to return data from candidates, number of votes and percentage of votes
+countVotes = 0
+cand = []
+votesNum = []
+votesPerc = []
+with open(pollpath, newline = "") as csvfile:
+csvreader = csv.reader(csvfile, delimiter = ",")
+csv_header = next(csvreader)
 
+for row in csvreader:
+    countVotes += 1
+    if row[2] not in cand:
+        cand.append(row[2])
+        index = cand.index(row[2])
+        votesNum.append(1)
+    else:
+        votesNum[cand.index(row[2])] += 1
+
+for row in votesNum:
+    calc = round((100*(row/countVotes)))
+    votesPerc.append(calc)
+
+winCand = cand[votesNum.index(max(votesNum))]
+#print results
+print("Election Results")
+print("-------------------------")
+print("Total Votes: " + str(countVotes))
+print("-------------------------")
+for x in range(len(cand)):
+    print(str(cand[x])+": "+"{:.3%}".format(votesPerc[x]/100)+" ("+str(votesNum[x])+")")
+print("-------------------------")
+print("Winner: "+ str(winCand))
+print("-------------------------")
+
+#write to file
+output = open("ElectionResults.txt", "w")
+output.write("Election Results"+"\n")
+output.write("-------------------------"+"\n")
+output.write("Total Votes: " + str(countVotes)+"\n")
+output.write("-------------------------"+"\n")
+for x in range(len(cand)):
+    output.write(str(cand[x])+": "+"{:.3%}".format(votesPerc[x]/100)+" ("+str(votesNum[x])+")"+"\n")
+output.write("-------------------------"+"\n")
+output.write("Winner: "+ str(winCand)+"\n")
+output.write("-------------------------"+"\n")
+output.close()
